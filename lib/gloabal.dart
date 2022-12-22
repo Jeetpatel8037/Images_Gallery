9164cb275844fd37920df37b4d5024cd2aea3de4
Jeetpatel8037 <pateljeet341@gmail.com>
2022-12-22 09:15:00 +0530

    first commit

---
+++ lib/gloabal.dart
@@ -0,0 +1,32 @@
+List MainPicsList = [
+  {"Place Name": "Goa Beches", "Place Pic": "assets/images/goa beches.jpg"},
+  {"Place Name": "Himalayas", "Place Pic": "assets/images/himalayas.jpg"},
+  {"Place Name": "Taj Mahal", "Place Pic": "assets/images/taj mahal.jpg"},
+];
+
+List insideSliderList = [
+  [
+    {"Place Name": "Goa Beches", "Place Pic": "assets/images/goa beches.jpg"},
+    {"Place Name": "Goa Beches", "Place Pic": "assets/images/goa beches 1.jpg"},
+    {"Place Name": "Goa Beches", "Place Pic": "assets/images/goa beches 2.jpg"},
+    {"Place Name": "Goa Beches", "Place Pic": "assets/images/goa beches 3.jpg"},
+    {"Place Name": "Goa Beches", "Place Pic": "assets/images/goa beches 4.jpg"},
+    {"Place Name": "Goa Beches", "Place Pic": "assets/images/goa beches 5.jpg"},
+  ],
+  [
+    {"Place Name": "Himalayas", "Place Pic": "assets/images/himalayas.jpg"},
+    {"Place Name": "Himalayas", "Place Pic": "assets/images/himalayas 1.jpg"},
+    {"Place Name": "Himalayas", "Place Pic": "assets/images/himalayas 2.jpg"},
+    {"Place Name": "Himalayas", "Place Pic": "Iassets/images/himalayas 3.jpg"},
+    {"Place Name": "Himalayas", "Place Pic": "assets/images/himalayas 4.jpg"},
+    {"Place Name": "Himalayas", "Place Pic": "assets/images/himalayas 5.jpg"},
+  ],
+  [
+    {"Place Name": "Taj Mahal", "Place Pic": "assets/images/taj mahal.jpg"},
+    {"Place Name": "Taj Mahal", "Place Pic": "assets/images/taj mahal 1.jpg"},
+    {"Place Name": "Taj Mahal", "Place Pic": "assets/images/taj mahal 2.jpg"},
+    {"Place Name": "Taj Mahal", "Place Pic": "assets/images/taj mahal 3.jpg"},
+    {"Place Name": "Taj Mahal", "Place Pic": "assets/images/taj mahal 4.jpg"},
+    {"Place Name": "Taj Mahal", "Place Pic": "assets/images/taj mahal 5.jpg"},
+  ]
+];
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
